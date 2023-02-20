68530226fdb5ea59187fae395b9afd7ea3bc76c5
vtseng-agillitics <109692153+vtseng-agillitics@users.noreply.github.com>
2023-02-20 11:06:39 -0500

    Update db_and_schema.tf

--- db_and_schema.tf
+++ db_and_schema.tf
@@ -1,10 +1,10 @@
 resource "snowflake_database" "archive_database"{
-  name = "x"
+  name = "ARCHIVE_DB"
 }
 
 resource "snowflake_schema" "archive_schema"{
   database = snowflake_database.archive_database.name
-  name = "x"
+  name = "ARCHIVE_SCHEMA"
   data_retention_days = 7
 }
 
